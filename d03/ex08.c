7ce4cf5207e35cb2766cb8cff795d32b28d7f77f
Denis <46903210+thedenisnikulin@users.noreply.github.com>
2020-08-18 15:05:42 +0300

    Fix school 42 rules violations in d03/ex08

--- d03/ex08.c
+++ d03/ex08.c
@@ -1,42 +1,5 @@
-#include <unistd.h>
-
+int ppow(int num, int p);
 int ft_atoi(char *str);
-
-void ft_putchar(char c) 
-{
-   // 1 - запись в stdout
-   // &c - адрес параметра c
-   // 1 - количество байт которое нужно записать (1 byte == 1 char)
-   write(1, &c, 1);  // функция из unistd.h
-}
-
-void ft_putnbr(int nb) 
-{
-    int nbcp;
-    int size;
-
-    size = 1;   /* инициализируем size (размер) с числом 1 (понадобится для умножения на 10) */
-    if (nb < 0)   /* проверяем nb на отрицательность */
-    {
-        /* если отрицательное, выводим минус и делаем число положительным */
-        ft_putchar('-');
-        nb = -nb;
-    }
-    nbcp = nb;  /* сохраняем копию nb */
-    while(nbcp >= 10)  /* пополняем size */
-    {
-        /* количество цифр в size в итоге будет равно количеству цифр в nb */
-        nbcp /= 10;
-        size *= 10;
-    }
-    nbcp = nb;  /* восстанавливаем nbcp */
-    while (size > 0)
-    {
-        ft_putchar((char) (nbcp / size) + '0'); /* выводим в stdout первую цифру числа, деля число на size */
-        nbcp %= size;  /* заносим в nbcp остаток от (nb / size), то есть все число кроме первой его цифры */
-        size /= 10;  /* делим size на 10, уменьшая "количество цифр" */
-    }
-};
 
 int ppow(int num, int p)
 {
@@ -55,13 +18,6 @@ int ppow(int num, int p)
         p--;
     }
     return (num);
-}
-
-int main(void)
-{
-    char str[] = "-123.45";  /* output: -123 */
-    ft_putnbr(ft_atoi(str));
-    return (0);
 }
 
 int ft_atoi(char *str)
@@ -131,3 +87,47 @@ int ft_atoi(char *str)
     return (number);
 }
 
+/** тест
+ * void ft_putchar(char c) 
+ * {
+ *    // 1 - запись в stdout
+ *    // &c - адрес параметра c
+ *    // 1 - количество байт которое нужно записать (1 byte == 1 char)
+ *    write(1, &c, 1);  // функция из unistd.h
+ * }
+ * 
+ * void ft_putnbr(int nb) 
+ * {
+ *     int nbcp;
+ *     int size;
+ * 
+ *     size = 1;   // инициализируем size (размер) с числом 1 (понадобится для умножения на 10)
+ *     if (nb < 0)   // проверяем nb на отрицательность
+ *     {
+ *         // если отрицательное, выводим минус и делаем число положительным
+ *         ft_putchar('-');
+ *         nb = -nb;
+ *     }
+ *     nbcp = nb;  // сохраняем копию nb
+ *     while(nbcp >= 10)  // пополняем size
+ *     {
+ *         // количество цифр в size в итоге будет равно количеству цифр в nb
+ *         nbcp /= 10;
+ *         size *= 10;
+ *     }
+ *     nbcp = nb;  // восстанавливаем nbcp
+ *     while (size > 0)
+ *     {
+ *         ft_putchar((char) (nbcp / size) + '0'); // выводим в stdout первую цифру числа, деля число на size
+ *         nbcp %= size;  // заносим в nbcp остаток от (nb / size), то есть все число кроме первой его цифры
+ *         size /= 10;  // делим size на 10, уменьшая "количество цифр"
+ *     }
+ * }
+ * 
+ * int main(void)
+ * {
+ *     char str[] = "-123.45";  // output: -123
+ *     ft_putnbr(ft_atoi(str));
+ *     return (0);
+ * }
+*/
